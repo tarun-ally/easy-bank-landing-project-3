a9fe12f77ec998de73d22cf1cfb02dd5ba0ff546
Tarun Dev <tarundev101@gmail.com>
2021-10-08 18:05:20 +0530

    giving box shadow to nav bar and refractoring its position

--- myjavascript.js
+++ myjavascript.js
@@ -1,8 +1,19 @@
 function myFunction(){
+    let burgerTag = document.querySelector(".close");
     var x=document.getElementById("mylink")
     if (x.style.display === "block"){
         x.style.display ="none";
-    }else{
-        x.style.display ="block";
+        burgerTag.src="./images/icon-hamburger.svg";
+
+       
+
     }
+    else{
+        x.style.display ="block";
+        burgerTag.src="./images/icon-close.svg";
+        const shadow = document.querySelector("#mylink");
+        shadow.style.boxShadow = "50px 60px 400px 0 hsl(233, 26%, 24%)"
+
+    }
+
 }
